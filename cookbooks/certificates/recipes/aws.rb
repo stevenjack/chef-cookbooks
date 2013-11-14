a9c38c5ff3af79f313191e4c01077273f9cc4be1
Simon Thulbourn <simon+github@thulbourn.com>
2013-11-14 22:37:53 +0000

    removes aws, pal recipes in favour of just including the right attribute file

--- cookbooks/certificates/recipes/aws.rb
+++
@@ -1,6 +0,0 @@
-# Author:: Simon Thulbourn(<simon.thulbourn@bbc.co.uk)
-# Cookbook Name:: certificates
-# Recipe:: aws
-#
-
-include_recipe 'certificates'
--- cookbooks/certificates/recipes/pal.rb
+++
@@ -1,6 +0,0 @@
-# Author:: Simon Thulbourn(<simon.thulbourn@bbc.co.uk)
-# Cookbook Name:: certificates
-# Recipe:: pal
-#
-
-include_recipe 'certificates'
