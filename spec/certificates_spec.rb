dac09cd435f434b2fbb21f062c89cbbeebe08bf6
Simon Thulbourn <simon+github@thulbourn.com>
2013-11-15 11:46:53 +0000

    fixes spec

--- spec/certificates_spec.rb
+++ spec/certificates_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'certificates::default' do
+describe 'certificates::add' do
   let(:chef_run) {
     ChefSpec::Runner.new do |node|
       node.set[:developer_certificate] = 'foo'
@@ -10,6 +10,24 @@ describe 'certificates::default' do
 
   it 'creates file' do
     expect(chef_run).to create_file_if_missing('/tmp/foo.pem').with(
+      content: 'foo',
+      user: 'root',
+      group: 'root',
+      mode: '0600'
+    )
+  end
+end
+
+describe 'certificates::remove' do
+  let(:chef_run) {
+    ChefSpec::Runner.new do |node|
+      node.set[:developer_certificate] = 'foo'
+      node.set[:certificates][:locations] = [ '/tmp/foo.pem' ]
+    end.converge(described_recipe)
+  }
+
+  it 'creates file' do
+    expect(chef_run).to delete_file('/tmp/foo.pem').with(
       content: 'foo',
       user: 'root',
       group: 'root',
