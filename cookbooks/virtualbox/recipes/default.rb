ab2da1039a9d5c4e6fe4ca15e37efd873c56bcb9
Simon Thulbourn <simon+github@thulbourn.com>
2013-12-04 16:15:59 +0000

    adds virtualbox guest additions cookbook

---
+++ cookbooks/virtualbox/recipes/default.rb
@@ -0,0 +1,35 @@
+# Author:: Simon Thulbourn(<simon.thulbourn@bbc.co.uk)
+# Cookbook Name:: virtualbox
+# Recipe:: default
+#
+
+# package 'dkms'
+package 'kernel-devel'
+
+execute 'build tools' do
+  command 'yum groupinstall development-tools'
+  returns [0, 1]
+end
+
+version = File.read('/root/.vbox_version')
+
+execute 'download additions' do
+  command "curl -L http://download.virtualbox.org/virtualbox/#{version}/VBoxGuestAdditions_#{version}.iso -o /root/VBoxGuestAdditions_#{version}.iso"
+end
+
+execute 'mount additions' do
+  command "mount -o loop /root/VBoxGuestAdditions_#{version}.iso /mnt"
+end
+
+execute 'install additions' do
+  command 'sh /mnt/VBoxLinuxAdditions.run --nox11'
+  returns [0, 1]
+end
+
+execute 'unmount additions' do
+  command 'umount /mnt'
+end
+
+execute 'remove additions' do
+  command "rm -rf /root/VBoxGuestAdditions_#{version}.iso"
+end
